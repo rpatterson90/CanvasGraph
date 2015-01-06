5554247901093ff286547ad002126ad1b02a39cf
rpatterson90 <rpatterson90@outlook.com>
2015-01-05 19:38:44 -0600

    Basic Functionality
    This will draw a line in the graph and adds some skeleton code to
    organize some of the things that still need to be done.
    
    It's still rough, there's some things that I'd like to see if make the
    code cleaner, like a transform matrix.
    
    After parsing formulas decently, I'd like to add tests, adjust the graph
    if the formula crosses the y-axis, animate on 1D to see speed, and give
    some rendering options for more of a grid, animations, etc.. (also give
    a list of common easing functions)

--- script/graph.js
+++ script/graph.js
@@ -1,32 +1,76 @@
 var height = 382;
 var width = 382;
 var padding = 30;
-var origin = {x:padding, y:padding + height};
-/*
-(padding, padding)
-(padding, padding + height)
-(padding + width, padding + height)
-(padding + width, padding)
-*/
-function init() {
-	var canvas = document.getElementById('graph');
-	var context = canvas.getContext('2d');
+
+function parseFormula(formula) {
+	return function(x) {
+		return x;
+	}
+}
+
+function setFormula(formula) {
+	drawFormula(formula);
+	animateFormula(formula, 0.0075);
+}
+
+function updateFormula() {
+	var formulaText = $("#formulaText").val();
+	var formula = parseFormula(formulaText);
 	
-	context.moveToPadded = function(x, y) {
-		this.moveTo(padding + x, padding + y);
+	setFormula(formula);
+}
+
+function drawFormula(formula) {
+	var context = createContext("line");
+	context.clearRect(0,0, width + padding, height + padding);
+	
+	context.beginPath();
+	context.moveToPadded(0, height * (1-formula(0)));
+	for (var x = 0; x <= 1.0; x += 0.001) 
+	{
+		y = formula(x);
+		context.lineToPadded(width * x, height * (1-y));
 	}
-	context.lineToPadded = function(x, y) {
-		this.lineTo(padding + x, padding + y);
+	context.strokeStyle = "blue";
+	context.stroke();
+}
+
+var animation;
+function animateFormula(formula, step, t) {
+	if (animation) {
+		cancelAnimationFrame(animation);
 	}
-	context.fillTextPadded = function(text, x, y) {
-		this.fillText(text, padding + x, padding + y);
+	if (!t) {
+		t = 0;
 	}
+	
+	var canvas = document.getElementById("animation");
+	var context = canvas.getContext('2d');
+	context.clearRect(0,0, canvas.width, canvas.height);
+	
+	y = formula(t);
+	context.beginPath();
+	context.arc(padding + width * t, padding + height * (1-y), 5, 0, 2 * Math.PI, false);
+	context.fillStyle = "green";
+	context.fill();
+	
+	animation = requestAnimationFrame(function() {
+		step = (t > 1 || t < 0) ? step * -1 : step;
+		animateFormula(formula, step, t + step);
+	});
+}
+
+function drawGraph() {
+	var context = createContext("graph");
 	
 	//draw edge
 	context.beginPath();
 	context.moveToPadded(0, 0);
 	context.lineToPadded(0, height);
 	context.lineToPadded(width, height);
+	//These two lines 'close' the graph
+	//context.lineToPadded(width, 0);
+	//context.lineToPadded(0, 0);
 	context.stroke();
 	
 	//draw markers
@@ -46,6 +90,27 @@ function init() {
 		context.stroke();
 		context.fillTextPadded((1 - i).toFixed(1), - 21, height * i + 3);
 	}
+}
+
+function createContext(canvasName) {
+	var canvas = document.getElementById(canvasName);
+	var context = canvas.getContext('2d');
+	context.moveToPadded = function(x, y) {
+		this.moveTo(padding + x, padding + y);
+	}
+	context.lineToPadded = function(x, y) {
+		this.lineTo(padding + x, padding + y);
+	}
+	context.fillTextPadded = function(text, x, y) {
+		this.fillText(text, padding + x, padding + y);
+	}
+	
+	return context;
+}
+
+function init() {
+	drawGraph();
+	setFormula(function(x) { return x * x; });
 }
 
 $(document).ready(init);
